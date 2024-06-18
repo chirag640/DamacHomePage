183be772e9412fb43154f66018544d486dd7625f
chirag640 <chaudharychirag640@gmail.com>
2024-06-18 13:30:15 +0530

    hero section done

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'hero-pattern': "url('/assets/Rectangle 122.jpeg')",
+      },
+      fontFamily: {
+        oswald: ['Oswald', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};

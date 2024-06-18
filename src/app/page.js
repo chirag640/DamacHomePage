561851ecde9bd19f47192b6650f6e22f898db7a9
chirag640 <chaudharychirag640@gmail.com>
2024-06-18 20:33:50 +0530

    completed home page

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,15 @@ import Gym from '@/../public/assets/gym.png';
 import FlotingPool from '@/../public/assets/flotingPool.jpeg';
 import SwimmingPool from '@/../public/assets/swimmingPool.jpeg';
 import Rooms from '@/../public/assets/rooms.jpeg';
+import Rectangle89 from "@/../public/assets/Rectangle89.jpeg"
+import Rectangle90 from "@/../public/assets/Rectangle90.jpeg"
+import Rectangle92 from "@/../public/assets/Rectangle92.jpeg"
+import Rectangle93 from "@/../public/assets/Rectangle93.jpeg"
+import { MdOutlineArrowBackIos } from "react-icons/md";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
 
+
+const images = [Rectangle89, Rectangle90, Rectangle92, Rectangle93];
 export default function Home() {
   const [visibleSlides, setVisibleSlides] = useState([0, 1]); // Initially show the first two slides
   const [slideTransition, setSlideTransition] = useState(false);
@@ -31,7 +39,7 @@ export default function Home() {
           return [prevVisibleSlides[1], nextIndex];
         });
         setSlideTransition(false);
-      }, 500); // Match this duration with the CSS transition duration
+      }, 500);
     }, 2500);
 
     return () => clearInterval(interval);
@@ -44,10 +52,28 @@ export default function Home() {
     { image: Gym, title: 'Gymnasium & Fitness' },
   ];
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextImage();
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
 
   return (
     <main>
-      <div className="relative md:px-40 md:pt-10 h-screen overflow-hidden">
+      <section className="relative md:px-40 md:pt-10 h-screen overflow-hidden">
         {/* Background Image and Gradient */}
         <div className="absolute inset-0 bg-hero-pattern bg-right bg-cover">
           <div className="absolute inset-0 bg-gradient-to-b from-[#0C2546] to-[#188DF9] opacity-75"></div>
@@ -104,7 +130,7 @@ export default function Home() {
             </p>
           </div>
         </div>
-      </div>
+      </section>
       {/* Responsive Section with Carousel for Mobile */}
       <section className="md:px-40 flex flex-col md:flex-row justify-between items-center pt-16 bg-[#F4F9FF]">
         <div className="carousel-container flex justify-around items-center w-full md:flex-row md:space-x-4 md:hidden">
@@ -149,6 +175,51 @@ export default function Home() {
         ))}
       </div>
       <p className='text-center w-3/4 text-[#686868] md:text-right'>*T&Cs apply | ** Based on similar branded projects in the last 2 years. Source 1 | Source 2</p>
+      </section>
+
+      <section className='pt-14 md:px-40 md:flex md:justify-center md:items-center md:flex-col' >
+        <div className='md:w-4/5 flex justify-around items-center md:justify-end md:gap-4 '>
+        <button className=" bg-[#00357B] border-2 border-[#00357B] text-white font-bold py-4 px-8 rounded-lg transition duration-300 hover:bg-white hover:text-[#00357B]">
+        EXTERIORS
+          </button>
+          <button className=" bg-transparent border-2 border-[#00357B] text-[#00357B] font-bold py-4 px-8 rounded-lg transition duration-300 hover:bg-[#00357B] hover:text-white">
+          INTERIORS
+          </button>
+        </div>
+        <div className="relative w-full md:w-4/5 flex justify-center items-center mb-6 mt-6">
+        <MdOutlineArrowBackIos
+          color='white'
+          className="absolute left-0 text-3xl cursor-pointer"
+          onClick={prevImage}
+        />
+        <div className="flex justify-center items-center">
+          <Image
+            src={images[currentIndex]}
+            alt="Carousel Image"
+            className="transition-transform duration-500 rounded-2xl md:object-cover"
+          />
+        </div>
+        <MdOutlineArrowForwardIos
+          color='white'
+          className="absolute right-0 text-3xl cursor-pointer"
+          onClick={nextImage}
+        />
+      </div>
+      <div className="flex flex-row justify-center space-x-4">
+        {images.map((image, index) => (
+          <div key={index} className={`w-40 h-40 ${index === currentIndex ? 'opacity-50' : ''}`}>
+            <Image
+              src={image}
+              alt={`Queue Image ${index}`}
+              onClick={() => setCurrentIndex(index)}
+              className="cursor-pointer rounded-lg"
+            />
+          </div>
+        ))}
+      </div>
+      </section>
+      <section className='h-20 mt-6 flex justify-center items-center bg-[#091D41]'>
+        <p className="text-white">© DAMAC Copyright 2024 All rights reserved.</p>
       </section>
     </main>
   );
